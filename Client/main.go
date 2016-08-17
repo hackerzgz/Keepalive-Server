aad3643ca2a19eabfffee6075dd91867c4d2d2d6
HackeZ <767110505@qq.com>
2016-08-17 11:09:58 +0800

    client done

--- Client/main.go
+++ Client/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 
 	utils "Keepalive-Server/Utils"
 )
@@ -34,7 +35,8 @@ func main() {
 	Wch = make(chan []byte)
 
 	host, port := utils.GetTCPConf("../TCPConf.json")
-	tcpAddr := host + ":" + string(port)
+	tcpAddr := host + ":" + strconv.Itoa(port)
+	fmt.Println("tcpAddr --> ", tcpAddr)
 	addr, err := net.ResolveTCPAddr("tcp", tcpAddr)
 	conn, err := net.DialTCP("tcp", nil, addr)
 
@@ -43,14 +45,78 @@ func main() {
 		log.Println("Conn Server Error -->", err.Error())
 		return
 	}
-	fmt.Println("Server Conn Done.")
+	log.Println("Server Conn Done.")
 	defer conn.Close()
 
 	go Handler(conn)
 	select {
 	case <-Dch:
-		fmt.Println("Conn Close!")
+		log.Println("Conn Close!")
 	}
 }
 
-func Handler(conn *net.TCPConn) {}
+// Handler Handle Conn Data.
+func Handler(conn *net.TCPConn) {
+	// 直到 register ok
+	data := make([]byte, 128)
+
+	for {
+		conn.Write([]byte{Req_REGISTER, '#', '2'})
+		conn.Read(data)
+
+		if data[0] == Res_REGISTER {
+			break
+		}
+	}
+
+	go RHandler(conn)
+	go WHandler(conn)
+	go Work()
+}
+
+// RHandler 心跳包，回复 ack.
+func RHandler(conn *net.TCPConn) {
+	for {
+		data := make([]byte, 128)
+
+		i, _ := conn.Read(data)
+		if i == 0 {
+			Dch <- true
+			return
+		}
+		if data[0] == Req_HEARTBEAT {
+			log.Println("Recv HeartBeat Pack")
+			conn.Write([]byte{Res_REGISTER, '#', 'h'})
+			log.Println("Send HeartBeat Pack Ack")
+		} else if data[0] == Req {
+			log.Println("Recv Data Pack")
+			fmt.Printf("%v\n", string(data[2:]))
+			Rch <- data[2:]
+			conn.Write([]byte{Res, '#'})
+		}
+	}
+
+}
+
+// WHandler 写入数据
+func WHandler(conn *net.TCPConn) {
+	for {
+		select {
+		case msg := <-Wch:
+			fmt.Println((msg[0]))
+			fmt.Println("Send Data After: " + string(msg[1:]))
+			conn.Write(msg)
+		}
+	}
+}
+
+// Work 读取数据到写入通道
+func Work() {
+	for {
+		select {
+		case msg := <-Rch:
+			fmt.Println("Work Recv " + string(msg))
+			Wch <- []byte{Req, '#', 'x', 'x', 'x', 'x', 'x'}
+		}
+	}
+}
